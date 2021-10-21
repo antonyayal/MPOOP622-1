271f8a014c60496de872ad8fdaaf746af18c2e10
Antonio Ayala <antony_ayal@hotmail.com>
2021-10-21 19:12:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+* @author Antonio Ayala
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+
+  /**
+  * @param  a Primer entero de la ecuación
+  * @param  b Segundo entero de la suma
+  * @return   La suma de los enteros a más b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
